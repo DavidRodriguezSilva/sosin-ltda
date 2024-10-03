110dc87f947a61c7070ba41615340b513debe825
David Rodriguez <drodriguez@ol4b.com>
2024-10-02 22:05:19 -0500

    wip: ajuste en la url base de la pagina

--- src/_app.page.tsx
+++ src/_app.page.tsx
@@ -1,28 +1,11 @@
-import { Link, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home/index.page"
 
 const App = () => {
   return (
-    <div className="App">
-      <div>
-        <nav>
-          <ul id="navigation">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="sosin-ldta">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
