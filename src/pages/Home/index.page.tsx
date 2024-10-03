ce72fc245dda2c0afe73d360a112489fc2c5ae0f
Daniel <danielrodriguezxt27@gmail.com>
2024-10-03 15:47:27 -0500

    feat: Creacion de nuevas secciones de la pagina, entre ellas Feature, About, Our Services and Gallery

--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,12 +1,20 @@
 import Header from "../../shared/components/header/Header"
 import Presentation from "./components/Presentation"
 import Footer from "../../shared/components/Footer/Footer"
+import Feature from "../../shared/components/Feature/Feature"
+import About from "../../shared/components/About/About"
+import ServiceSection from "../../shared/components/Service Section/ServiceSection"
+import Gallery from "../../shared/components/Gallery/Gallery"
 
 export const Home = () => {
   return (
     <>
       <Header />
       <Presentation />
+      <Feature/>
+      <About/>
+      <ServiceSection/>
+      <Gallery/>
       <Footer/>
     </>
   )
